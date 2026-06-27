14b4d907c19debb64e9b4fe33961d4b083ef0d08
Faizyar Ali <shaikhfaizy84@gmail.com>
2026-06-27 18:24:11 +0500

    refactor: move learning code into the project and add initial client data fetch

--- saas-frontend/src/App.tsx
+++ saas-frontend/src/App.tsx
@@ -1,25 +1,48 @@
-import Card from './component/card.jsx'
-import jobOpenings from './data.jsx'
+import {useState,useEffect} from 'react'
+
+type Client = {
+  id: number,
+  email: string,
+  name: string,
+  country: string,
+};
 
 
 function App(){
-  return(
-    <div className="parent">
-      {jobOpenings.map((elem,index) => (
-        <div key={index}>
-        <Card
-          company={elem.company}
-          logo_url={elem.logo_url}
-          postedAgo={elem.postedAgo}
-          title={elem.title}
-          jobType={elem.JobType}
-          salary={elem.salary}
-          location={elem.location}
-        />
-        </div>
-      ))}
-    </div>
-  );
-}
+  const [clients, setClients] = useState<Client[]>([]);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(()=>{
+    async function fetchClient(){
+      try {
+        const res = await fetch(
+          "http://127.0.0.1:8000/clients?user_id=1&limit=20&offset=0" 
+        );
+        const data = await res.json();
+        setClients(data);
+      }catch (err){
+        console.log("Error fetching clients: ",err);
+      }finally{
+        setLoading(false);
+      }
+    }
+    fetchClient();
+  },[]);
+  return (
+    <div>
+      <h1>Clients</h1>
+    {loading ? (
+      <p>Loading...</p>
+    ):(
+      clients.map((client) => (
+        <div key={client.id} style={{ marginBottom: "10px" }}>
+          <h3>{client.name}</h3>
+          <p>{client.email}</p>
+          <p>{client.country}</p>
+        </div>
+      ))
+    )}
+  </div>
+  )
+}
 export default App
